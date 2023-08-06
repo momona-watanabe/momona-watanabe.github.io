73477208a4616c859e51e51c100d4a1ed40f1b05
momona-watanabe <140260256+momona-watanabe@users.noreply.github.com>
2023-08-06 22:22:19 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*ハンバーガーメニュー*/
 $(".openbtn").click(function(){
     $(this).toggleClass('active');
     $(".gnav-sp").toggleClass('panelactive');
@@ -8,3 +8,53 @@ $(".gnav-sp a").click(function(){
     $(".openbtn").removeClass('active');
     $(".gnav-sp").removeClass('panelactive');
 });
+
+
+
+
+
+
+/*loadingアニメーション*/
+$(window).on('load',function(){
+  $("#splash").delay(150).fadeOut('slow',function(){
+      $('body').addClass('appear');
+  });
+ 
+  $('.splashbg').on('animationend', function() {    
+      
+  });
+      
+});
+
+
+
+
+
+/*fadeupアニメーション*/
+function fadeAnime(){
+
+  $('.fadeUpTrigger').each(function(){ 
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    }else{
+    $(this).removeClass('fadeUp');
+    }
+    });
+}
+
+  $(window).scroll(function (){
+    fadeAnime();
+  });
+
+
+
+
+
+
+
+
+
+
